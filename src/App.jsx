3e41946c2a9aeacd27be1261fe208133418c9327
Phong Thanh <tangthanh.phong99@gmail.com>
2024-11-08 16:14:47 +0700

    chore: complete base

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { EmployeeList } from './components';
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<EmployeeList />} />
+        
+      </Routes>
+    </>
+
+  )
+}
+
+export default App
---
+++ src/components/EmployeeList.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const EmployeeList = () => {
+  return (
+    <div>
+      <h2>Employeelist</h2>
+      
+    </div>
+  )
+}
+
+export default EmployeeList
